d712a9f3b97bb4d7895a40c1fcee387dde71e7e6
Miyukivv <natalia.kielarr@interia.pl>
2025-03-21 01:06:51 +0100

    zmiana nazwy projektu

--- SpringTask1/src/main/java/org/example/IVehicleRepository.java
+++ SpringTask1/src/main/java/org/example/IVehicleRepository.java
@@ -5,8 +5,8 @@ import java.util.List;
 public interface IVehicleRepository {
      public List<Vehicle>  getVehicles();
 
-    public boolean rentVehicle(String registrationPlate); //nie wiem czy tu dobrze
-    public boolean returnVehicle(String registrationPlate); //nie wiem czy tu dobrze
+    public boolean rentVehicle(String registrationPlate);
+    public boolean returnVehicle(String registrationPlate); 
 
     public void save(String filename, Vehicle Vehicle_to_save);
     public void showVehicles();
